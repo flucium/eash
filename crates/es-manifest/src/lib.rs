b4fdf0affb970b8390e41393b6e961e18b8e217f
flucium <14249224+flucium@users.noreply.github.com>
2022-12-11 22:00:36 +0900

    add ./crates/es-manifest

---
+++ crates/es-manifest/src/lib.rs
@@ -0,0 +1,23 @@
+const EASH_NAME: &str = "eash";
+
+const EASH_VERSION: &str = "0.0.1";
+
+const EASH_LINK: &str = "github.com/flucium/eash";
+
+const EASH_AUTHOR: &str = "flucium";
+
+pub fn name()->String{
+    EASH_NAME.to_owned()
+}
+
+pub fn version()->String{
+    EASH_VERSION.to_owned()
+}
+
+pub fn link()->String{
+    EASH_LINK.to_owned()
+}
+
+pub fn author()->String{
+    EASH_AUTHOR.to_owned()
+}
