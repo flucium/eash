cea17016cdd49522f6cbe729501ba038be9551ac
flucium <14249224+flucium@users.noreply.github.com>
2022-12-26 16:43:56 +0900

    add ./crates/es/parser/src/token.rs

--- crates/es-parser/src/token.rs
+++ crates/es-parser/src/token.rs
@@ -4,33 +4,82 @@ use std::fmt::Result;
 
 #[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
 pub enum Token {
-    String(String), //
-    Ident(String),  //
+    Pipe, // |
+
+    Assign, // =
+
+    Equal,    // ==
+    NotEqual, // !=
+
+    Gt, // >
+    Lt, // <
+
+    OR,  // ||
+    AND, // &&
+
+    Semicolon, // ;
+    Comma,     // ,
+    Bang,      // !
+    Dollar,    // $
+    Ampersand, // &
+
+    LParen, // (
+    RParen, // )
+
+    LBrace, // {
+    RBrace, // }
+
+    Def, // def
+
+    If,   // if
+    Elif, // elif
+    Else, // else
+
+    Loop, // loop
+
+    Return, // return
+
+    True,  // true
+    False, // false
+
+    String(String), // hello
+    Ident(String),  // $a , &b
     Number(isize),  // 0 ~ 9
     FD(u32),        // 0 ~ 9
-    Ampersand,      // &
-    Dollar,         // $
-    Equal,          // =
-    Pipe,           // |
-    Gt,             // >
-    Lt,             // <
-    Semicolon,      // ;
 }
 
 impl Display for Token {
     fn fmt(&self, tkn: &mut Formatter) -> Result {
         match self {
-            Token::String(string) => write!(tkn, "{string}"),
-            Token::Ident(string) => write!(tkn, "{string}"),
-            Token::Number(n) => write!(tkn, "{n}"),
-            Token::FD(n) => write!(tkn, "{n}"),
-            Token::Equal => write!(tkn, "="),
-            Token::Ampersand => write!(tkn, "&"),
-            Token::Dollar => write!(tkn, "$"),
+            Token::Pipe => write!(tkn, "|"),
+            Token::Assign => write!(tkn, "="),
+            Token::Equal => write!(tkn, "=="),
+            Token::NotEqual => write!(tkn, "!="),
             Token::Gt => write!(tkn, ">"),
             Token::Lt => write!(tkn, "<"),
-            Token::Pipe => write!(tkn, "|"),
+            Token::OR => write!(tkn, "||"),
+            Token::AND => write!(tkn, "&&"),
             Token::Semicolon => write!(tkn, ";"),
+            Token::Comma => write!(tkn, ","),
+            Token::Bang => write!(tkn, "!"),
+            Token::Dollar => write!(tkn, "$"),
+            Token::Ampersand => write!(tkn, "&"),
+            Token::LParen => write!(tkn, "("),
+            Token::RParen => write!(tkn, ")"),
+            Token::LBrace => write!(tkn, "{{"),
+            Token::RBrace => write!(tkn, "}}"),
+            Token::Def => write!(tkn, "def"),
+            Token::If => write!(tkn, "if"),
+            Token::Elif => write!(tkn, "elif"),
+            Token::Else => write!(tkn, "else"),
+            Token::Loop => write!(tkn, "loop"),
+            Token::Return => write!(tkn, "return"),
+            Token::True => write!(tkn, "true"),
+            Token::False => write!(tkn, "false"),
+            Token::String(v) => write!(tkn, "{v}"),
+            Token::Ident(v) => write!(tkn, "{v}"),
+            Token::Number(v) => write!(tkn, "{v}"),
+            Token::FD(v) => write!(tkn, "{v}"),
         }
     }
 }
