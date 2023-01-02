81d278aef4d8e5a68a65fa44782d55289c9238f0
flucium <14249224+flucium@users.noreply.github.com>
2023-01-02 15:03:53 +0900

    update ./crates/es-ast/src/lib.rs

--- crates/es-ast/src/lib.rs
+++ crates/es-ast/src/lib.rs
@@ -13,6 +13,30 @@ pub struct Loop {
     block: Block,
 }
 
+impl Loop {
+    pub fn new(comparison: Comparison) -> Self {
+        Self {
+            comparison: comparison,
+            block: Block {
+                left: None,
+                right: None,
+            },
+        }
+    }
+
+    pub fn insert(&mut self, block: Block) {
+        self.block = block;
+    }
+
+    pub fn comparison(&self) -> &Comparison {
+        &self.comparison
+    }
+
+    pub fn block(&self) -> &Block {
+        &self.block
+    }
+}
+
 #[derive(Debug)]
 pub struct If {
     comparison: Comparison,
@@ -20,16 +44,104 @@ pub struct If {
     child: Option<Box<Self>>,
 }
 
+impl If {
+    pub fn new(comparison: Comparison) -> Self {
+        Self {
+            comparison: comparison,
+            block: Block {
+                left: None,
+                right: None,
+            },
+            child: None,
+        }
+    }
+
+    pub fn insert_block(&mut self, block: Block) {
+        self.block = block;
+    }
+
+    pub fn insert_child(&mut self, child: If) {
+        if let Some(self_child) = self.child.as_mut() {
+            self_child.insert_child(child)
+        } else {
+            self.child = Some(Box::new(child))
+        }
+    }
+
+    pub fn comparison(&self) -> &Comparison {
+        &self.comparison
+    }
+
+    pub fn block(&self) -> &Block {
+        &self.block
+    }
+
+    pub fn child(&self) -> Option<&If> {
+        match &self.child {
+            Some(child) => Some(&child),
+            None => None,
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Assign {
-    left: Expression,
-    right: Expression,
+    identify: Expression,
+    expr: Expression,
+}
+
+impl Assign {
+    pub fn new(identify: Expression, expr: Expression) -> Self {
+        Self {
+            identify: identify,
+            expr: expr,
+        }
+    }
+
+    pub fn identify(&self) -> &Expression {
+        &self.identify
+    }
+
+    pub fn expr(&self) -> &Expression {
+        &self.expr
+    }
 }
 
 #[derive(Debug)]
 pub struct Block {
     left: Option<Box<Statement>>,
     right: Option<Box<Statement>>,
+}
+
+impl Block {
+    pub fn new() -> Self {
+        Self {
+            left: None,
+            right: None,
+        }
+    }
+
+    pub fn insert_left(&mut self, statement: Statement) {
+        self.left = Some(Box::new(statement));
+    }
+
+    pub fn insert_right(&mut self, statement: Statement) {
+        self.right = Some(Box::new(statement));
+    }
+
+    pub fn left(&self) -> Option<&Statement> {
+        match &self.left {
+            Some(left) => Some(&left),
+            None => None,
+        }
+    }
+
+    pub fn right(&self) -> Option<&Statement> {
+        match &self.right {
+            Some(right) => Some(&right),
+            None => None,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -50,10 +162,58 @@ pub struct Command {
     suffix: Option<CommandSuffix>,
 }
 
+impl Command {
+    pub fn new(prefix: Expression) -> Self {
+        Self {
+            prefix: Box::new(prefix),
+            suffix: None,
+        }
+    }
+
+    pub fn insert_suffix(&mut self, suffix: CommandSuffix) {
+        self.suffix = Some(suffix);
+    }
+
+    pub fn prefix(&self) -> &Expression {
+        &self.prefix
+    }
+
+    pub fn suffix(&mut self) -> Option<&CommandSuffix> {
+        match &self.suffix {
+            Some(suffix) => Some(suffix),
+            None => None,
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct CommandSuffix {
     expr: Box<Expression>,
     suffix: Option<Box<Self>>,
+}
+
+impl CommandSuffix {
+    pub fn new(expr: Expression) -> Self {
+        Self {
+            expr: Box::new(expr),
+            suffix: None,
+        }
+    }
+
+    pub fn insert(&mut self, suffix: CommandSuffix) {
+        self.suffix = Some(Box::new(suffix))
+    }
+
+    pub fn expr(&self) -> &Expression {
+        &self.expr
+    }
+
+    pub fn suffix(&self) -> Option<&CommandSuffix> {
+        match &self.suffix {
+            Some(suffix) => Some(&suffix),
+            None => None,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -61,6 +221,28 @@ pub struct Redirect {
     kind: RedirectKind,
     left: Box<Expression>,
     right: Box<Expression>,
+}
+
+impl Redirect {
+    pub fn new(kind: RedirectKind, left: Expression, right: Expression) -> Self {
+        Self {
+            kind: kind,
+            left: Box::new(left),
+            right: Box::new(right),
+        }
+    }
+
+    pub fn kind(&self) -> &RedirectKind {
+        &self.kind
+    }
+
+    pub fn left(&self) -> &Expression {
+        &self.left
+    }
+
+    pub fn right(&self) -> &Expression {
+        &self.right
+    }
 }
 
 #[derive(Debug)]
@@ -73,6 +255,37 @@ pub enum RedirectKind {
 pub struct Pipe {
     left: Option<Box<Expression>>,
     right: Option<Box<Expression>>,
+}
+
+impl Pipe {
+    pub fn new() -> Self {
+        Self {
+            left: None,
+            right: None,
+        }
+    }
+
+    pub fn insert_left(&mut self, expr: Expression) {
+        self.left = Some(Box::new(expr));
+    }
+
+    pub fn insert_right(&mut self, expr: Expression) {
+        self.right = Some(Box::new(expr));
+    }
+
+    pub fn left(&self) -> Option<&Expression> {
+        match &self.left {
+            Some(left) => Some(&left),
+            None => None,
+        }
+    }
+
+    pub fn right(&self) -> Option<&Expression> {
+        match &self.right {
+            Some(right) => Some(&right),
+            None => None,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -94,3 +307,4 @@ pub enum Comparison {
         right: Box<Expression>,
     },
 }
+
