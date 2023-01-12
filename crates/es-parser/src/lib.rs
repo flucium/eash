73b26017fcbef29d002259837d568265c582d266
flucium <14249224+flucium@users.noreply.github.com>
2023-01-12 15:16:29 +0900

    update ./crates/es-parser/src/lib.rs
    i wrote a dumb code. but it works

--- crates/es-parser/src/lib.rs
+++ crates/es-parser/src/lib.rs
@@ -4,64 +4,182 @@ pub mod token;
 use error::*;
 use es_ast::*;
 use lexer::*;
-use std::iter::Peekable;
-use std::mem;
 use token::*;
 
+pub fn parse() {
+    let lexer = &mut Lexer::new("a $A = hello").peekable();
+}
+
 pub struct Parser {
-    lexer: Peekable<Lexer>,
+    lexer: Lexer,
 }
 
 impl Parser {
     pub fn new(lexer: Lexer) -> Self {
-        Self {
-            lexer: lexer.peekable(),
+        Self { lexer: lexer }
+    }
+
+    pub fn parse(&mut self) {}
+
+    pub fn parse_assign(&mut self) {}
+
+    pub fn parse_command(&mut self) {
+        println!("{:?}", self.parse_command_prefix());
+    }
+
+    fn parse_command_suffix(&mut self) -> Result<Option<CommandSuffix>> {
+        // return if token is Pipe || Semicolon || EOL || EOF
+        if matches!(
+            self.lexer.peek(),
+            None | Some(Token::Pipe) | Some(Token::Semicolon) | Some(Token::EOL) | Some(Token::EOF)
+        ) {
+            return Ok(None);
+        }
+
+        let mut suffix = CommandSuffix::new();
+
+        while let Some(token) = self.lexer.next() {
+            if let Ok(fd) = parse_fd(&token) {
+                match self.lexer.peek() {
+                    Some(Token::Gt) => {
+                        self.lexer.consume();
+
+                        match self.lexer.next() {
+                            None => Err(Error::new(ErrorKind::Unknown, "".to_owned()))?,
+                            Some(token) => {
+                                match parse_string(&token)
+                                    .or(parse_number(&token).or(parse_fd(&token)))
+                                {
+                                    Err(err) => Err(err)?,
+                                    Ok(expr) => {
+                                        suffix.insert(Expression::Redirect(Redirect::new(
+                                            RedirectKind::Write,
+                                            fd,
+                                            expr,
+                                        )));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    Some(Token::Lt) => {
+                        self.lexer.consume();
+
+                        match self.lexer.next() {
+                            None => Err(Error::new(ErrorKind::Unknown, "".to_owned()))?,
+                            Some(token) => {
+                                match parse_string(&token)
+                                    .or(parse_number(&token).or(parse_fd(&token)))
+                                {
+                                    Err(err) => Err(err)?,
+                                    Ok(expr) => {
+                                        suffix.insert(Expression::Redirect(Redirect::new(
+                                            RedirectKind::Read,
+                                            fd,
+                                            expr,
+                                        )));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    _ => {
+                        suffix.insert(fd);
+                    }
+                }
+            } else {
+                if let Ok(expr) = parse_variable(&token) {
+                    suffix.insert(expr);
+                } else {
+                    match token {
+                        Token::Ampersand => {
+                            suffix.insert(Expression::Background(true));
+                            break;
+                        }
+
+                        _ => {
+                            suffix.insert(Expression::String(token.to_string()));
+                        }
+                    }
+                }
+            }
+
+
+            if self.lexer.next_is(&Token::Pipe)
+                || self.lexer.next_is(&Token::Semicolon)
+                || self.lexer.next_is(&Token::EOL)
+                || self.lexer.next_is(&Token::EOF)
+            {
+                break;
+            }
+        }
+
+        Ok(Some(suffix))
+    }
+
+    fn parse_command_prefix(&mut self) -> Result<Expression> {
+        match self.lexer.peek() {
+            None => Err(Error::new(ErrorKind::Unknown, "".to_owned()))?,
+
+            Some(token) => {
+                let expr = parse_variable(token).or(parse_string(token).or(parse_number(token)));
+
+                match expr {
+                    Err(err) => Err(err),
+
+                    Ok(expr) => {
+                        self.lexer.consume();
+
+                        Ok(expr)
+                    }
+                }
+            }
         }
     }
 }
 
-fn parse_fd<F>(f: F) -> Result<Expression>
-where
-    F: FnOnce() -> Option<Token>,
-{
-    match f() {
-        Some(Token::FD(number)) => Ok(Expression::FD(number)),
+// fn next_token_is_eof(lexer: &mut Peekable<Lexer>) -> bool {
+//     lexer.peek() == Some(&Token::EOF)
+// }
+
+// fn next_token_is_eol(lexer: &mut Peekable<Lexer>) -> bool {
+//     lexer.peek() == Some(&Token::EOL)
+// }
+
+// fn next_token_is_semicolon(lexer: &mut Peekable<Lexer>) -> bool {
+//     lexer.peek() == Some(&Token::Semicolon)
+// }
+
+// fn next_token_is_pipe(lexer: &mut Peekable<Lexer>) -> bool {
+//     lexer.peek() == Some(&Token::Pipe)
+// }
+
+fn parse_fd(token: &Token) -> Result<Expression> {
+    match token {
+        Token::FD(fd) => Ok(Expression::FD(fd.to_owned())),
         _ => Err(Error::new(ErrorKind::Unknown, "".to_owned())),
     }
 }
 
-fn parse_number<F>(f: F) -> Result<Expression>
-where
-    F: FnOnce() -> Option<Token>,
-{
-    match f() {
-        Some(Token::Number(number)) => Ok(Expression::Number(number)),
+fn parse_number(token: &Token) -> Result<Expression> {
+    match token {
+        Token::Number(number) => Ok(Expression::Number(number.to_owned())),
         _ => Err(Error::new(ErrorKind::Unknown, "".to_owned())),
     }
 }
 
-fn parse_variable<F>(f: F) -> Result<Expression>
-where
-    F: FnOnce() -> Option<Token>,
-{
-    match f() {
-        Some(Token::Ident(string)) => Ok(Expression::Variable(string)),
+fn parse_variable(token: &Token) -> Result<Expression> {
+    match token {
+        Token::Ident(string) => Ok(Expression::Variable(string.to_owned())),
         _ => Err(Error::new(ErrorKind::Unknown, "".to_owned())),
     }
 }
 
-fn parse_string<F>(f: F) -> Result<Expression>
-where
-    F: FnOnce() -> Option<Token>,
-{
-    match f() {
-        Some(Token::String(string)) => Ok(Expression::String(string)),
+fn parse_string(token: &Token) -> Result<Expression> {
+    match token {
+        Token::String(string) => Ok(Expression::String(string.to_owned())),
         _ => Err(Error::new(ErrorKind::Unknown, "".to_owned())),
     }
-}
-
-fn next_token_if_eq(lexer: &mut Peekable<Lexer>, token: Token) -> Option<Token> {
-    lexer.next_if(|tkn| mem::discriminant(tkn) == mem::discriminant(&token))
 }
 
 // #[macro_export]
